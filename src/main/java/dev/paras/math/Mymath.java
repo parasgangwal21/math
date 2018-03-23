8f48ecc509f69b3c69980ffae0b3f595b9853124
Paras <parasgangwal21@gmail.com>
2018-03-23 08:50:18 +0530

    Initial commit

---
+++ src/main/java/dev/paras/math/Mymath.java
@@ -0,0 +1,16 @@
+package dev.paras.math;
+
+/**
+ * Created by Paras on 23-03-2018.
+ */
+
+public class Mymath {
+    public static int plus(int a,int b)
+    {
+        return a+b;
+    }
+    public static int multi(int a,int b)
+    {
+        return a*b;
+    }
+}
